ddb5149dcef51bbfbcba972b050acb2975dcb3cd
Haofei Yu <1125027232@qq.com>
2024-05-22 02:17:00 -0400

    polish and change the code

--- data/sotopia_rm_format_convert.py
+++ data/sotopia_rm_format_convert.py
@@ -3,35 +3,60 @@ from pprint import pprint
 
 import jsonlines
 
-with jsonlines.open("sotopia_pi_openai_log_attribution.jsonl", "r") as reader:
-    dataset = list(reader)
 
-rewards = []
-prompts = []
-for data in dataset:
-    agent_name = data["agent"]
-    goal_score = data["goal_score"]
-    goal = data["goal"]
-    dialogues = []
-    dialogue = ""
+def calc_reward(utter, goal_score):
+    utter_attrib = utter[1]
+    if utter_attrib == -1:
+        reward = -1
+    else:
+        reward = utter_attrib / 3 * goal_score
+    return reward
 
-    goal_description = f"The goal of the agent is {goal}."
-    for speaker, utter in data["attributed_utterances"].items():
-        if agent_name in speaker:
-            dialogue += f"{speaker}: {utter[0]}\n"
-            utter_attrib = utter[1]
-            if utter_attrib == -1:
-                reward = -1
-            else:
-                reward = utter_attrib / 3 * goal_score
-            prompt = f"{dialogue}\n{goal_description}\nFocus on the last utterance. What is the reward for the last utterance for achieving the goal? {reward}\n"
-            prompts.append(prompt)
-            rewards.append(reward)
 
-formulated_dataset = []
-for prompt, reward in zip(prompts, rewards):
-    data = {"instruction": prompt, "input": "", "output": "", "value": reward}
-    formulated_dataset.append(data)
+if __name__ == "__main__":
 
-with open("sotopia_pi_utterance_reward.json", "w") as writer:
-    json.dump(formulated_dataset, writer, indent=4)
+    with jsonlines.open(
+        "sotopia_pi_openai_log_attribution.jsonl", "r"
+    ) as reader:
+        dataset = list(reader)
+
+    rewards = []
+    historys = []
+    systems = []
+    prompts = []
+    for data in dataset:
+        agent_name = data["agent"]
+        goal_score = data["goal_score"]
+        scenario = data["scenario"]
+        goal = data["goal"]
+        history = []
+
+        for speaker, utter in data["attributed_utterances"].items():
+            if agent_name in speaker:
+                systems.append(
+                    f"The scenario is {scenario}. The goal of {agent_name} is {goal}."
+                )
+                prompts.append(
+                    f'How much does "{speaker} {utter[0]}" contribute to the goal of {agent_name}?'
+                )
+                history += [f"{speaker} {utter[0]}"]
+                historys.append(history)
+                reward = calc_reward(utter, goal_score)
+                rewards.append(reward)
+
+    formulated_dataset = []
+    for prompt, reward, system, history in zip(
+        prompts, rewards, systems, historys
+    ):
+        data = {
+            "instruction": prompt,
+            "input": "",
+            "output": "",
+            "value": reward,
+            "system": system,
+            "history": history,
+        }
+        formulated_dataset.append(data)
+
+    with open("sotopia_pi_utterance_reward.json", "w") as writer:
+        json.dump(formulated_dataset, writer, indent=4)
