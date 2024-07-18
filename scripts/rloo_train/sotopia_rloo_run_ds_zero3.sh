2224455654095d9d2653fa7b3d331825ffa7b94e
lwaekfjlk <1125027232@qq.com>
2024-07-18 06:13:01 -0400

    update poetry

---
+++ scripts/rloo_train/sotopia_rloo_run_ds_zero3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NPROC_PER_NODE=4
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
+    --nproc_per_node $NPROC_PER_NODE \
+    --nnodes 1 \
+    --standalone \
+    src/train.py examples/lora_sotopia/mistral_lora_sotopia_rloo_ds.yaml
+# jbsub -mem 80g -cores 20+4 -q alt_24h -require h100 -out ./mistral_lora_sotopia_rloo_ds.log -err ./mistral_lora_sotopia_rloo_ds.err bash ./scripts/rloo_train/sotopia_rloo_run_ds_zero3.sh
