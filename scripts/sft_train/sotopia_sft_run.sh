bf574111ce38ce5472abe6b4ec92f8f17dfeae8f
lwaekfjlk <1125027232@qq.com>
2024-07-18 04:11:55 -0400

    update setting for lora sft

--- scripts/sft_train/sotopia_sft_run.sh
+++ scripts/sft_train/sotopia_sft_run.sh
@@ -1,1 +1,2 @@
 python -m src.llmtuner.cli train examples/lora_sotopia/mistral_lora_sotopia_sft.yaml
+# jbsub -mem 80g -cores 20+4 -q alt_24h -require h100 -out ./mistral_lora_sotopia_sft.log -err ./mistral_lora_sotopia_sft.err bash ./scripts/sft_train/mistral_lora_sotopia_sft.sh
