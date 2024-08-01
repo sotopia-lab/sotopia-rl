3e2601dd60a755afb4e0fedd98422c97d558d770
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-08-01 00:02:25 -0700

    Inference on preference data (#49)
    * add preference data
    
    * finished code for generating preference data
    
    * added ignore
    
    * added preference data and code
    
    * fixed mypy
    
    * fixed poetry
    
    * reverted for LLM train
    
    * added updated preference data
    
    * fixeed prediction code
    
    * staging before changing preference data
    
    * finished inference on preference
    
    * finished selfplay and ppo training
    
    * fixed merge error
    
    ---------
    
    Co-authored-by: lwaekfjlk <1125027232@qq.com>

---
+++ scripts/preference/preference_prediction.sh
@@ -0,0 +1,1 @@
+python -m src.llmtuner.cli train examples/lora_sotopia/llama3_lora_sotopia_rm_predict.yaml
--- scripts/rm_train/sotopia_rm_run.sh
+++ scripts/rm_train/sotopia_rm_run.sh
@@ -1,1 +1,2 @@
-python -m src.llmtuner.cli train examples/lora_sotopia/mistral_lora_sotopia_rm_key_utter.yaml
+# python -m src.llmtuner.cli train examples/lora_sotopia/mistral_lora_sotopia_rm_key_utter.yaml
+python -m src.llmtuner.cli train examples/lora_sotopia/llama3_lora_sotopia_ppo.yaml
