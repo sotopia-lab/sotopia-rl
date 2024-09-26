f36eb83bb77699c18c3390ae35a054359bb3dfd3
lwaekfjlk <1125027232@qq.com>
2024-09-26 18:34:15 +0000

    add independent eval

--- evals/sotopia_eval.sh
+++ evals/sotopia_eval.sh
@@ -1,17 +1,27 @@
-python -m vllm.entrypoints.openai.api_server --model google/gemma-2-2b-it --chat-template gemma-it.jinja
+python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --chat-template /workspace/sotopia-rl/evals/mistral-instruct.jinja
 
 
-python examples/experiment_eval.py \
+python sotopia_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
 '--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
-'--gin.AGENT1_MODEL="custom/google/gemma-2-2b-it@http://localhost:8000/v1"' \
+'--gin.AGENT1_MODEL="custom/workspace/gemma-2-2b-it@http://localhost:8000/v1"' \
 '--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo"'
+'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo-0926"'
 
+python sotopia_eval.py \
+--gin_file sotopia_conf/generation_utils_conf/generate.gin \
+--gin_file sotopia_conf/server_conf/server.gin \
+--gin_file sotopia_conf/run_async_server_in_batch.gin \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8"]' \
+'--gin.AGENT1_MODEL="custom/workspace/gemma-2-2b-it@http://localhost:8000/v1"' \
+'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+'--gin.BATCH_SIZE=1' \
+'--gin.PUSH_TO_DB=True' \
+'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo-0926"'
 
 
 python -m vllm.entrypoints.openai.api_server --model google/gemma-2-2b --chat-template mistral-instruct.jinja --served-model-name sotopia_gemma-2-2b-sft --enable-lora --lora-modules sft=gemma-2-2b-sft
