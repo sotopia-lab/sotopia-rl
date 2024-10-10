7caf54633f9e93d924af29f8b120b65499dc2ad1
Haofei Yu <haofeiy@cs.cmu.edu>
2024-10-10 18:15:48 -0500

    feat(eval): support sotopia-eval with vllm==0.6.2 (#65)
    * update with comprehensive yaml
    
    * support vllm==0.6.2
    
    * update eval
    
    * delete ueseless scripts

--- evals/sotopia_eval.sh
+++ evals/sotopia_eval.sh
@@ -1,7 +1,8 @@
-python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja 
 
 
-python sotopia_eval.py \
+
+poetry run python examples/experiment_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
@@ -10,9 +11,31 @@ python sotopia_eval.py \
 '--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="gpt-3.5-turbo_vs_gpt-3.5-turbo-0927"'
+'--gin.TAG="gpt-3.5-turbo_vs_gpt-3.5-turbo-1003"'
 
-python sotopia_eval.py \
+poetry run python examples/experiment_eval.py \
+--gin_file sotopia_conf/generation_utils_conf/generate.gin \
+--gin_file sotopia_conf/server_conf/server.gin \
+--gin_file sotopia_conf/run_async_server_in_batch.gin \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
+'--gin.AGENT1_MODEL="gpt-4o-mini"' \
+'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+'--gin.BATCH_SIZE=1' \
+'--gin.PUSH_TO_DB=True' \
+'--gin.TAG="gpt-4o-mini_vs_gpt-3.5-turbo-1003"'
+
+poetry run python examples/experiment_eval.py \
+--gin_file sotopia_conf/generation_utils_conf/generate.gin \
+--gin_file sotopia_conf/server_conf/server.gin \
+--gin_file sotopia_conf/run_async_server_in_batch.gin \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
+'--gin.AGENT1_MODEL="gpt-4o"' \
+'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+'--gin.BATCH_SIZE=1' \
+'--gin.PUSH_TO_DB=True' \
+'--gin.TAG="gpt-4o_vs_gpt-3.5-turbo-1003"'
+
+poetry run python examples/experiment_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
@@ -21,43 +44,65 @@ python sotopia_eval.py \
 '--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo-0926"'
+'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo-1003"'
 
-python sotopia_eval.py \
---gin_file sotopia_conf/generation_utils_conf/generate.gin \
---gin_file sotopia_conf/server_conf/server.gin \
---gin_file sotopia_conf/run_async_server_in_batch.gin \
-'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8"]' \
-'--gin.AGENT1_MODEL="custom/workspace/gemma-2-2b-it@http://localhost:8000/v1"' \
-'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
-'--gin.BATCH_SIZE=1' \
-'--gin.PUSH_TO_DB=True' \
-'--gin.TAG="gemma-2-2b-it_vs_gpt-3.5-turbo-0926"'
+# =====================================================
+# experiments scripts that need to run starts from here
+# =====================================================
 
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --port 8000 --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja  --served-model-name gemma-2-2b-it --enable-lora --lora-modules sotopia_gemma-2-2b-it-sft=/workspace/sotopia-rl/saves/gemma-2-2b-it/sft_ckpt_270
 
-python -m vllm.entrypoints.openai.api_server --model google/gemma-2-2b --chat-template gemma-2-2b-it.jinja --served-model-name sotopia_gemma-2-2b-sft --enable-lora --lora-modules sft=gemma-2-2b-sft
-
-python examples/experiment_eval.py \
+poetry run python examples/experiment_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
 '--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
-'--gin.AGENT1_MODEL="custom/sft@http://localhost:8000/v1"' \
-'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+'--gin.AGENT1_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8000/v1"' \
+'--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8000/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="sotopia_gemma-2-2b-sft_vs_gpt-3.5-turbo"'
+'--gin.TAG="sotopia_gemma-2-2b-it-sft_vs_sotopia_gemma-2-2b-it-sft-1010"'
 
 
-python -m vllm.entrypoints.openai.api_server --model google/gemma-2-2b --chat-template gemma-2-2b-it.jinja --served-model-name sotopia_gemma-2-2b-ppo --enable-lora --lora-modules ppo10=gemma-2-2b-ppo/checkpoint-10
+CUDA_VISIBLE_DEVICES=1 python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --port 8005 --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja --served-model-name gemma-2-2b-it --enable-lora --disable-sliding-window --max-model-len 4096 --lora-modules sotopia_gemma-2-2b-it-sft-ppo-rm-baseline=/workspace/sotopia-rl/saves/gemma-2-2b-it/sft_ppo_baseline_rm_ckpt_50
 
-python examples/experiment_eval.py \
+poetry run python examples/experiment_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
-'--gin.ENV_IDS=["01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNV13MHN97GAH73E3KM8"]' \
-'--gin.AGENT1_MODEL="custom/ppo10@http://localhost:8000/v1"' \
-'--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
+'--gin.AGENT1_MODEL="custom/sotopia_gemma-2-2b-it-sft-ppo-rm-baseline@http://localhost:8005/v1"' \
+'--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8000/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="sotopia_gemma-2-2b-ppo-10_vs_gpt-3.5-turbo"'
+'--gin.TAG="sotopia_gemma-2-2b-it-sft-ppo-rm-baseline_vs_sotopia_gemma-2-2b-it-sft-1010_v2"'
+
+
+
+CUDA_VISIBLE_DEVICES=1 python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --port 8005 --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja --served-model-name gemma-2-2b-it --enable-lora --disable-sliding-window --max-model-len 4096 --lora-modules sotopia_gemma-2-2b-it-sft-ppo-rm-key=/workspace/sotopia-rl/saves/gemma-2-2b-it/sft_ppo_key_rm_ckpt_50
+
+poetry run python examples/experiment_eval.py \
+--gin_file sotopia_conf/generation_utils_conf/generate.gin \
+--gin_file sotopia_conf/server_conf/server.gin \
+--gin_file sotopia_conf/run_async_server_in_batch.gin \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
+'--gin.AGENT1_MODEL="custom/sotopia_gemma-2-2b-it-sft-ppo-rm-key@http://localhost:8005/v1"' \
+'--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8000/v1"' \
+'--gin.BATCH_SIZE=1' \
+'--gin.PUSH_TO_DB=True' \
+'--gin.TAG="sotopia_gemma-2-2b-it-sft-ppo-rm-key_vs_sotopia_gemma-2-2b-it-sft-1010"'
+
+
+
+CUDA_VISIBLE_DEVICES=1 python -m vllm.entrypoints.openai.api_server --model /workspace/gemma-2-2b-it --port 8005 --chat-template /workspace/sotopia-rl/evals/gemma-2-2b-it.jinja --served-model-name gemma-2-2b-it --enable-lora --disable-sliding-window --max-model-len 4096 --lora-modules sotopia_gemma-2-2b-it-sft-ppo-rm-direct=/workspace/sotopia-rl/saves/gemma-2-2b-it/sft_ppo_direct_rm_ckpt_50
+
+poetry run python examples/experiment_eval.py \
+--gin_file sotopia_conf/generation_utils_conf/generate.gin \
+--gin_file sotopia_conf/server_conf/server.gin \
+--gin_file sotopia_conf/run_async_server_in_batch.gin \
+'--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
+'--gin.AGENT1_MODEL="custom/sotopia_gemma-2-2b-it-sft-ppo-rm-direct@http://localhost:8005/v1"' \
+'--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8000/v1"' \
+'--gin.BATCH_SIZE=1' \
+'--gin.PUSH_TO_DB=True' \
+'--gin.TAG="sotopia_gemma-2-2b-it-sft-ppo-rm-direct_vs_sotopia_gemma-2-2b-it-sft-1010"'
