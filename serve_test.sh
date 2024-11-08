88c26fec819a9847333860fff0510e2d735cfd3b
Haofei Yu <haofeiy@cs.cmu.edu>
2024-11-08 03:32:00 -0600

    fix(serve): fix misalignment with vllm inference (#78)

--- serve_test.sh
+++ serve_test.sh
@@ -1,9 +1,27 @@
 python manage.py start_with_config \
-    --sft_model_path "/data/haofeiy2/sotopia-rl/saves/sft/checkpoint-4000" \
+    --sft_model_path "/data/haofeiy2/sotopia-rl/saves/llama_factory_sft/checkpoint-270" \
     --reward_model_path "/data/haofeiy2/sotopia-rl/saves/rm_baseline/checkpoint-14000" \
-    --model_name "/data/haofeiy2/gemma-2-2b-it/" \
+    --model_name "/data/models/gemma-2-2b-it/" \
+    --template_path "/data/haofeiy2/sotopia-rl/evals/gemma-2-2b-it.jinja" \
+    --max_responses 1 \
+    --max_length 4096 \
+    --port 8001
+
+python manage.py start_with_config \
+    --sft_model_path "/data/haofeiy2/sotopia-rl/saves/llama_factory_sft/checkpoint-270" \
+    --reward_model_path "/data/haofeiy2/sotopia-rl/saves/rm_baseline/checkpoint-14000" \
+    --model_name "/data/models/gemma-2-2b-it/" \
     --template_path "/data/haofeiy2/sotopia-rl/evals/gemma-2-2b-it.jinja" \
     --max_responses 5 \
+    --max_length 4096 \
+    --port 8001
+
+python manage.py start_with_config \
+    --sft_model_path "/data/haofeiy2/sotopia-rl/saves/llama_factory_sft/checkpoint-270" \
+    --reward_model_path "/data/haofeiy2/sotopia-rl/saves/rm_baseline/checkpoint-14000" \
+    --model_name "/data/models/gemma-2-2b-it/" \
+    --template_path "/data/haofeiy2/sotopia-rl/evals/gemma-2-2b-it.jinja" \
+    --max_responses 10 \
     --max_length 4096 \
     --port 8001
 
--- serves/sotopia/views.py
+++ serves/sotopia/views.py
@@ -9,12 +9,15 @@ from rest_framework.decorators import api_view
 @api_view(["POST"])
 def chat_completions(request):
     messages = request.data.get("messages")
+    temperature = request.data.get("temperature", 1.0)
+    stream = request.data.get("stream", False)
+    n = request.data.get("n", 1)
     if not messages:
         return JsonResponse({"error": "Messages are required."}, status=400)
 
     # Access the globally loaded RejectionSampler instance
     sampler = apps.get_app_config("sotopia").rejection_sampler
-    top_response = sampler.inference(messages)
+    top_response = sampler.inference(messages, temperature, stream, n)
 
     if top_response is not None:
         # Format the response to mimic OpenAI's chat completion response
