e7b9278b40fd3eaf52fb3075a91e9538c738581a
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2025-02-11 23:54:17 -0800

    [feat]Reorganizing Reward Prompting (#93)
    * WIP; need to rewrite reverse_engineering
    
    * WIP; need to rewrite reverse_engineering

--- scripts/annotate/sample_episodes_and_annotate.py
+++ scripts/annotate/sample_episodes_and_annotate.py
@@ -3,21 +3,20 @@ import sys
 sys.path.append("../../")
 import argparse
 
-from sotopia_rl.prompting.attribution_prompting import generate_reward_attribution
+from sotopia_rl.prompter.attribution_prompting import generate_reward_attribution
 from sotopia_rl.utils.preprocess import add_score
 
-
-def main(data_dir: str, llm_name: str) -> None:
+def main(data_dir: str, llm_name: str, input_file: str, output_file: str) -> None:
     add_score(
         data_dir,
-        "sotopia_pi_episodes.jsonl",
+        input_file,
         "sotopia_pi_episodes_with_scores.jsonl",
     )
     generate_reward_attribution(
         data_dir,
         llm_name=llm_name,
         input_file="sotopia_pi_episodes_with_scores.jsonl",
-        output_file="sotopia_pi_openai_log_attribution.jsonl",
+        output_file=output_file,
     )
 
 
@@ -35,7 +34,20 @@ if __name__ == "__main__":
         required=True,
         help="Name of the language model",
     )
+    parser.add_argument(
+        "--input_file",
+        type=str,
+        required=True,
+        help="Input file containing episodes",
+    )
+    parser.add_argument(
+        "--output_file",
+        type=str,
+        required=True,
+        help="Output file containing episodes with reward attribution",
+    )
+    
 
     args = parser.parse_args()
 
-    main(args.data_dir, args.llm_name)
+    main(args.data_dir, args.llm_name, args.input_file, args.output_file)
