63e0060f4e1ab53e5c045730e46bf59048553822
lwaekfjlk <1125027232@qq.com>
2025-02-23 15:22:28 +0000

    update

--- evals/haofei_eval_commands.sh
+++ evals/haofei_eval_commands.sh
@@ -34,12 +34,7 @@ poetry run python examples/experiment_eval.py \
 '--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8005/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-<<<<<<< HEAD
 '--gin.TAG="sotopia_rejection-sampling-rm-direct-prompt-and-sft_vs_sotopia_gemma-2-2b-it-sft-1204_v2_sample_20"'
-=======
-'--gin.TAG="sotopia_rejection-sampling-rm-direct-prompt-and-sft_vs_sotopia_gemma-2-2b-it-sft-1203_sample_20"'
-
->>>>>>> 4cd3d6a03b7d23ef240b8bb0838e90de02f38fc8
 
 
 
@@ -47,13 +42,9 @@ poetry run python examples/experiment_eval.py \
 
 
 
-<<<<<<< HEAD
 # ============================
 # switch agent1 and agent2
 # ============================
-=======
-# switch agent1 and agent2
->>>>>>> 4cd3d6a03b7d23ef240b8bb0838e90de02f38fc8
 
 # with sotopia-rl env, launch the rejection sampling model
 CUDA_VISIBLE_DEVICES=4,5 python manage.py start_with_config \
@@ -61,11 +52,7 @@ CUDA_VISIBLE_DEVICES=4,5 python manage.py start_with_config \
     --reward_model_path "/data/haofeiy2/sotopia-rl/saves/rm_direct_prompt/checkpoint-14000" \
     --model_name "/data/models/gemma-2-2b-it/" \
     --template_path "/data/haofeiy2/sotopia-rl/evals/gemma-2-2b-it.jinja" \
-<<<<<<< HEAD
     --max_responses 40 \
-=======
-    --max_responses 1 \
->>>>>>> 4cd3d6a03b7d23ef240b8bb0838e90de02f38fc8
     --max_length 4096 \
     --sft_batch_size 10 \
     --rm_batch_size 3 \
@@ -87,16 +74,8 @@ poetry run python examples/experiment_eval.py \
 --gin_file sotopia_conf/server_conf/server.gin \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
 '--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHN7A1ZX5KSMT2YN9RXC4", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
-<<<<<<< HEAD
 '--gin.AGENT1_MODEL="custom/sotopia_rejection_sampling@http://localhost:8002/sotopia"' \
 '--gin.AGENT2_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8006/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
 '--gin.TAG="sotopia_rejection-sampling-rm-direct-prompt-and-sft_vs_sotopia_gemma-2-2b-it-sft-1204_sample_40"'
-=======
-'--gin.AGENT1_MODEL="custom/sotopia_gemma-2-2b-it-sft@http://localhost:8006/v1"' \
-'--gin.AGENT2_MODEL="custom/sotopia_rejection_sampling@http://localhost:8002/sotopia"' \
-'--gin.BATCH_SIZE=1' \
-'--gin.PUSH_TO_DB=True' \
-'--gin.TAG="sotopia_rejection-sampling-rm-direct-prompt-and-sft_vs_sotopia_gemma-2-2b-it-sft-1203_sample_1"'
->>>>>>> 4cd3d6a03b7d23ef240b8bb0838e90de02f38fc8
