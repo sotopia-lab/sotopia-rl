80b29749e81605cba5425e90941e09ab7511c360
lwaekfjlk <1125027232@qq.com>
2025-03-08 18:26:52 +0000

    update

---
+++ scripts/test_episode_num.py
@@ -0,0 +1,14 @@
+import json
+
+with open('../data/sotopia_pi_bc_episodes_reward_o3-mini.json') as f:
+    dataset = json.load(f)
+
+settings = []
+for data in dataset:
+    parts = data['instruction'].split('\n')
+    # part[8] is scenario, part[9] is agent combo
+    setting = parts[8] + parts[9]
+    if setting not in settings:
+        settings.append(setting) 
+print(len(dataset))
+print(len(settings))
