fe478909e8344513c5a927759c145d0d1e5a96c6
Haofei Yu <haofeiy@cs.cmu.edu>
2025-03-16 16:25:17 -0500

    feat(ppo): rewrite ppo (#108)
    * update
    
    * update

--- scripts/train_ppo.sh
+++ scripts/train_ppo.sh
@@ -1,11 +1,20 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 poetry run python /data/haofeiy2/sotopia-rl/scripts/train_ppo.py \
+CUDA_VISIBLE_DEVICES=0,2,6,9 poetry run python /data/haofeiy2/sotopia-rl/scripts/train_ppo.py \
   --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
-  --reward_model_name /data/haofeiy2/sotopia-rl/rm_direct_overfit/checkpoint-100 \
-  --batch_size 1 \
-  --num_epochs 1 \
+  --value_model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
+  --reward_model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
+  --policy_adapter_path /data/haofeiy2/sotopia-rl/sft_qwen25_7b/checkpoint-4495 \
+  --ref_adapter_path /data/haofeiy2/sotopia-rl/sft_qwen25_7b/checkpoint-4495 \
+  --reward_adapter_path /data/haofeiy2/sotopia-rl/rm_direct_o3_mini/checkpoint-4000 \
+  --value_adapter_path /data/haofeiy2/sotopia-rl/rm_direct_o3_mini/checkpoint-4000 \
+  --policy_use_qlora \
+  --reward_use_qlora \
+  --value_use_qlora \
+  --batch_size 2 \
+  --num_epochs 100 \
   --ppo_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_gpt4_sft_overfit.json \
   --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja\
   --ppo_epochs 4 \
   --gamma 0.99 \
   --lam 0.95 \
+  --use_lora \
   --checkpoint_dir /data/haofeiy2/sotopia-rl/ppo_qwen25_7b
