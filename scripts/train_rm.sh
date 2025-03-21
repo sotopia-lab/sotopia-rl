360e5bffcc7af9485d8503bb6fbd44e068c799a2
lwaekfjlk <1125027232@qq.com>
2025-03-21 20:29:10 +0000

    update

--- scripts/train_rm.sh
+++ scripts/train_rm.sh
@@ -9,8 +9,7 @@ CUDA_VISIBLE_DEVICES=9 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_goal_progress_gpt-4o_cleaned.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_goal_progress_gpt-4o_cleaned \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_goal_progress_gpt-4o_cleaned
 
 CUDA_VISIBLE_DEVICES=5 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -23,8 +22,7 @@ CUDA_VISIBLE_DEVICES=5 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_o3-mini_attribution_direct.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_direct_o3_mini \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_direct_o3_mini
 
 CUDA_VISIBLE_DEVICES=9 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -37,8 +35,7 @@ CUDA_VISIBLE_DEVICES=9 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_discounting.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_discounting \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_discounting
 
 CUDA_VISIBLE_DEVICES=8 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -51,8 +48,7 @@ CUDA_VISIBLE_DEVICES=8 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 100 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_direct_average.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_direct_average \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_direct_average
 
 CUDA_VISIBLE_DEVICES=7 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -65,8 +61,7 @@ CUDA_VISIBLE_DEVICES=7 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_goal_progress_gpt-4o.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_goal_progress_gpt_4o \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_goal_progress_gpt_4o
 
 CUDA_VISIBLE_DEVICES=6 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -79,8 +74,7 @@ CUDA_VISIBLE_DEVICES=6 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_only_response_gpt-4o.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_only_response_gpt_4o \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_reward_only_response_gpt_4o
 
 CUDA_VISIBLE_DEVICES=1 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
@@ -93,8 +87,7 @@ CUDA_VISIBLE_DEVICES=1 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
 --evaluation_steps 500 \
 --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_5-scale_gpt-4o.json \
 --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
---checkpoint_dir /data/haofeiy2/sotopia-rl/rm_direct_gpt4o \
---use_qlora
+--checkpoint_dir /data/haofeiy2/sotopia-rl/rm_direct_gpt4o
 
 
 CUDA_VISIBLE_DEVICES=9 python /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
