c20c673b7402291eae142cead2dcc03a58cbbbcf
Haofei Yu <haofeiy@cs.cmu.edu>
2025-03-22 15:48:05 -0500

    feat(serve): support vllm for response generation (#132)
    * update
    
    * update

--- evals/rej_sampling_num5.sh
+++ evals/rej_sampling_num5.sh
@@ -4,10 +4,10 @@ python examples/experiment_eval.py \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
 '--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
 '--gin.AGENT1_MODEL="custom/sotopia_rejection_sampling-num5@http://localhost:8002/sotopia"' \
-'--gin.AGENT2_MODEL="custom/qwen25-7b-instruct-sft-gpu4@http://localhost:8005/v1"' \
+'--gin.AGENT2_MODEL="custom/qwen25-7b-instruct-sft-gpu2@http://localhost:8005/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="SFT_RM_o3mini_rejection_sampling_num5_vs_qwen25-7b-instruct-sft-0320-v4"' \
+'--gin.TAG="SFT_RM_o3mini_0322_rejection_sampling_num5_vs_qwen25-7b-instruct-sft-0322-v2"' \
 && \
 python examples/experiment_eval.py \
 --gin_file sotopia_conf/generation_utils_conf/generate.gin \
@@ -15,7 +15,7 @@ python examples/experiment_eval.py \
 --gin_file sotopia_conf/run_async_server_in_batch.gin \
 '--gin.ENV_IDS=["01H7VFHNV13MHN97GAH73E3KM8", "01H7VFHN5WVC5HKKVBHZBA553R", "01H7VFHN9W0WAFZCBT09PKJJNK", "01H7VFHPDZVVCDZR3AARA547CY", "01H7VFHPQQQY6H4DNC6NBQ8XTG", "01H7VFHN7WJK7VWVRZZTQ6DX9T", "01H7VFHPS5WJW2694R1MNC8JFY", "01H7VFHNN7XTR99319DS8KZCQM", "01H7VFHQ11NAMZS4A2RDGDB01V", "01H7VFHPSWGDGEYRP63H2DJKV0", "01H7VFHNF4G18PC9JHGRC8A1R6", "01H7VFHNNYH3W0VRWVY178K2TK", "01H7VFHP8AN5643B0NR0NP00VE", "01H7VFHN7A1ZX5KSMT2YN9RXC4"]' \
 '--gin.AGENT2_MODEL="custom/sotopia_rejection_sampling-num5@http://localhost:8002/sotopia"' \
-'--gin.AGENT1_MODEL="custom/qwen25-7b-instruct-sft-gpu4@http://localhost:8005/v1"' \
+'--gin.AGENT1_MODEL="custom/qwen25-7b-instruct-sft-gpu2@http://localhost:8005/v1"' \
 '--gin.BATCH_SIZE=1' \
 '--gin.PUSH_TO_DB=True' \
-'--gin.TAG="SFT_RM_o3mini_rejection_sampling_num5_vs_qwen25-7b-instruct-sft-0320-v4"'
+'--gin.TAG="SFT_RM_o3mini_0322_rejection_sampling_num5_vs_qwen25-7b-instruct-sft-0322-v2"'
