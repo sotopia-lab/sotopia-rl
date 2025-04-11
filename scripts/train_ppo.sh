7e20ee83806e0251c9908abf0340de3b56aa9783
lwaekfjlk <1125027232@qq.com>
2025-04-11 13:24:12 +0000

    update

--- scripts/train_ppo.sh
+++ scripts/train_ppo.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1,7,8 accelerate launch \
+CUDA_VISIBLE_DEVICES=1,6,7 accelerate launch \
   --config_file /data/haofeiy2/sotopia-rl/scripts/accelerate_config.yaml \
   --main_process_port 29511 \
   /data/haofeiy2/sotopia-rl/scripts/train_ppo.py \
@@ -7,10 +7,12 @@ CUDA_VISIBLE_DEVICES=1,7,8 accelerate launch \
   --ref_adapter_path /data/haofeiy2/sotopia-rl/sft_qwen25_7b_sft_round_1_bc_data_top_2/checkpoint-1500 \
   --reward_adapter_path /data/haofeiy2/sotopia-rl/rm_reward_direct_default_without_that_n_error_as_the_end/checkpoint-4480 \
   --value_adapter_path /data/haofeiy2/sotopia-rl/rm_reward_direct_default_without_that_n_error_as_the_end/checkpoint-4480 \
-  --learning_rate 1e-5 \
+  --learning_rate 5e-6 \
+  --max_length 4096 \
   --per_device_train_batch_size 1 \
   --per_device_eval_batch_size 1 \
   --gradient_accumulation_steps 1 \
+  --local_rollout_forward_batch_size 1 \
   --num_mini_batches 1 \
   --ppo_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_round1_qwen_sft_all_with_instruct_string.json \
   --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
@@ -18,4 +20,5 @@ CUDA_VISIBLE_DEVICES=1,7,8 accelerate launch \
   --num_train_epochs 5 \
   --gamma 0.99 \
   --lam 0.95 \
-  --output_dir /data/haofeiy2/sotopia-rl/ppo_origin_qwen25_7b_reward_direct_default_no_goal_gpt-4o_without_goal_leak_with_sft_self_play_data_use_sotopia_pi_full_data_0408
+  --output_dir /data/haofeiy2/sotopia-rl/ppo_origin_qwen25_7b_reward_direct_default_no_goal_gpt-4o_without_goal_leak_with_sft_self_play_data_use_sotopia_pi_full_data_0408 \
+  --use_lora_train_ppo
--- scripts/train_sft.py
+++ scripts/train_sft.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_epochs", type=int, default=3, help="Number of training epochs")
     parser.add_argument("--sft_data_path", type=str, required=True, help="Path to SFT data")
     parser.add_argument("--template_path", type=str, required=True, help="Path to the Jinja template file")
-    parser.add_argument("--max_length", type=int, default=4096, help="Max sequence length")
+    parser.add_argument("--max_length", type=int, default=3000, help="Max sequence length")
     parser.add_argument("--weight_decay", type=float, default=0.01, help="Weight decay")
     parser.add_argument("--evaluation_steps", type=int, default=100, help="Evaluation interval in steps")
     parser.add_argument("--accumulation_steps", type=int, default=1, help="Gradient accumulation steps")
