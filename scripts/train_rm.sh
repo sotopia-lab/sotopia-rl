b9f243b627d5219e0e011bfbbe038444a9acfa3d
Haofei Yu <haofeiy@cs.cmu.edu>
2025-04-17 21:53:06 -0500

    fix(rm, sft): fix some old errors (#170)
    * update
    
    * support

--- scripts/train_rm.sh
+++ scripts/train_rm.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0,5 accelerate launch \
-  --config_file /data/haofeiy2/sotopia-rl-eval/sotopia-rl/scripts/accelerate_config_ppo.yaml \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 accelerate launch \
+  --config_file /data/haofeiy2/sotopia-rl/scripts/accelerate_config_rm.yaml \
   --main_process_port 29530 \
   /data/haofeiy2/sotopia-rl-eval/sotopia-rl/scripts/train_rm.py \
   --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
-  --learning_rate 1e-6 \
+  --learning_rate 1e-5 \
   --max_length 4096 \
   --train_batch_size 1 \
   --val_batch_size 1 \
-  --accumulation_steps 1 \
+  --accumulation_steps 8 \
   --num_epochs 30 \
-  --evaluation_steps 200 \
-  --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_all_the_same.json \
+  --evaluation_steps 100 \
+  --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_token_length.json \
   --template_path /data/haofeiy2/sotopia-rl-eval/sotopia-rl/evals/qwen2.5-7b.jinja \
-  --checkpoint_dir /data/haofeiy2/sotopia-rl-eval/sotopia-rl/rm_test
+  --checkpoint_dir /data/haofeiy2/sotopia-rl-eval/sotopia-rl/rm_token_length
