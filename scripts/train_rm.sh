2b74e159efd4e883a69a30230174ff8a87dfb947
lwaekfjlk <1125027232@qq.com>
2025-04-18 03:11:25 +0000

    update

--- scripts/train_rm.sh
+++ scripts/train_rm.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 accelerate launch \
   --config_file /data/haofeiy2/sotopia-rl/scripts/accelerate_config_rm.yaml \
-  --main_process_port 29530 \
-  /data/haofeiy2/sotopia-rl-eval/sotopia-rl/scripts/train_rm.py \
+  --main_process_port 29500 \
+  /data/haofeiy2/sotopia-rl/scripts/train_rm.py \
   --model_name /mnt/data_from_server1/models/Qwen2.5-7B-Instruct \
   --learning_rate 1e-5 \
   --max_length 4096 \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 accelerate launch \
   --val_batch_size 1 \
   --accumulation_steps 8 \
   --num_epochs 30 \
-  --evaluation_steps 100 \
+  --evaluation_steps 5 \
   --reward_data_path /data/haofeiy2/sotopia-rl/data/sotopia_pi_bc_episodes_reward_token_length.json \
-  --template_path /data/haofeiy2/sotopia-rl-eval/sotopia-rl/evals/qwen2.5-7b.jinja \
-  --checkpoint_dir /data/haofeiy2/sotopia-rl-eval/sotopia-rl/rm_token_length
+  --template_path /data/haofeiy2/sotopia-rl/evals/qwen2.5-7b.jinja \
+  --checkpoint_dir /data/haofeiy2/sotopia-rl/rm_token_length
