b0d7e34a9987ca8267011e346196bdc233ce4272
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2025-04-20 15:50:29 -0700

    feat(eval): Sotopia Pi data processing integration (#180)
    * finished sotopia-pi integration
    
    * feature(rm): support accelerator for RM (#159)
    
    * fix(ppo): fix errors in PPO training  (#155)
    
    * feature(ppo): support deepspeed of ppo
    
    * support accelerator
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update server
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fnally see the correct curve for reawrd
    
    * update
    
    * update for trying to switch to ppotrainer v1 but failed
    
    * update
    
    * updata
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: robotMonkeyButler <zhaoyining5445@gmail.com>
    
    * feat(sft): support accelerator sft (#160)
    
    * feature(ppo): support deepspeed of ppo
    
    * support accelerator
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update server
    
    * update
    
    ---------
    
    Co-authored-by: robotMonkeyButler <zhaoyining5445@gmail.com>
    
    * fix(rm): support rm without \n at the end and end with </s> (#157)
    
    * feature(ppo): support deepspeed of ppo
    
    * support accelerator
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update server
    
    * update
    
    * run other rm
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: robotMonkeyButler <zhaoyining5445@gmail.com>
    
    * fix(ppo): clean and support running ppo scripts (#161)
    
    * feat(poetry): update poetry dependency (#162)
    
    * fix(ppo): guarantee to frozen all rm param (#163)
    
    * update
    
    * update
    
    * feat(ppo): optimize ppo training (#164)
    
    * update
    
    * update
    
    * update
    
    * update
    
    * feat(ppo): support lora or not for ppo (#165)
    
    * update
    
    * update
    
    * feature(grpo): support grpo (#168)
    
    * feature(grpo): support grpo
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix(grpo): fix grpo trainer (#169)
    
    * feature(grpo): support grpo
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix(grpo): fix grpo trainer
    
    ---------
    
    Co-authored-by: Haofei Yu <haofeiy@cs.cmu.edu>
    
    * fix(rm, sft): fix some old errors (#170)
    
    * update
    
    * support
    
    * fix(rm): use get_peft_model to support lora training (#171)
    
    * update
    
    * support
    
    * update
    
    * update
    
    * feat(sft): update sft trainer for latest TRL (#172)
    
    * feat(poetry): fix trl version (#173)
    
    * update
    
    * update
    
    * feat(rm): wrap up new rm trainer (#175)
    
    * bug(rm): inference not correct
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: robotMonkeyButler <zhaoyining5445@gmail.com>
    
    * feat(eval): Judge Model Ablation Code (#154)
    
    * finished switching side
    
    * fix
    
    * temp staging
    
    * added judge ablation
    
    * removed untitled
    
    * reverted experiment eval
    
    * reverted
    
    ---------
    
    Co-authored-by: Yining Zhao <140395411+robotMonkeyButler@users.noreply.github.com>
    Co-authored-by: Haofei Yu <haofeiy@cs.cmu.edu>
    Co-authored-by: robotMonkeyButler <zhaoyining5445@gmail.com>

---
+++ scripts/data_process/generate_sft_from_episodes.py
@@ -0,0 +1,47 @@
+import click
+import json
+import os
+import glob
+from typing import Any, Dict, List
+from tqdm import tqdm
+from db_free_reverse_engineering import run_reverse_by_pk_agent
+
+@click.command()
+@click.option("--data_dir", type=str, required=True, help="Directory containing data files.")
+@click.option("--utterances_output_subdir", type=str, required=True, help="Directory to save the utterances.")
+@click.option("--episodes_file", type=str, required=True, help="Path to the raw JSON file.")
+@click.option("--sft_output_file", type=str, required=False, help="Path to the processed JSON file.")
+def main(data_dir: str, utterances_output_subdir: str, episodes_file: str, sft_output_file: str) -> None:
+    episode_path = os.path.join(data_dir, episodes_file)
+    if not os.path.exists(episode_path):
+        raise Exception(f"Episodes file not found: {episode_path}")
+    
+    with open(episode_path, 'r') as f:
+        data: List[Dict[str, Any]] = [json.loads(d) for d in f.readlines()]
+    # breakpoint()
+    cache_dir = os.path.join(data_dir, utterances_output_subdir)
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+        for d in tqdm(data):
+            run_reverse_by_pk_agent(d['episode_id'], True, cache_dir, episode_path)
+            run_reverse_by_pk_agent(d['episode_id'], False, cache_dir, episode_path)
+    
+    utterances = []
+    for record in glob.glob(f"{cache_dir}/*.json"):
+        with open(record, 'r') as f:
+            uttr = json.load(f)
+            utterances.append(uttr)
+    
+    sft_utterances = []
+    for uttr in utterances:
+        sft_utterances.append({
+            "input": uttr['prompt'],
+            "output": uttr['result'],
+        })
+    
+    print(f"Total utterances: {len(sft_utterances)}")
+    with open(os.path.join(data_dir, sft_output_file), 'w') as f:
+        json.dump(sft_utterances, f, indent=4)
+
+if __name__ == "__main__":
+    main()
